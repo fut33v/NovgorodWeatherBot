90f7ccbe95001501056a3968bd80e8418424069e
fut33v <fat32nov@gmail.com>
2015-11-10 22:49:06 +0300

    Weather.com forecast added

--- novgorod_weather_bot.py
+++ novgorod_weather_bot.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from util import bot_util
-from novgorod_forecast import novgorod_forecast
+#from novgorod_forecast import novgorod_forecast
+from novgorod_forecast.novgorod_forecast import YandexForecaster
+from novgorod_forecast.novgorod_forecast import WeatherComForecaster
 from novgorod_weather import novgorod_weather
 from telegram_bot.telegram_bot import TelegramBot
 
@@ -8,6 +10,7 @@ __author__ = 'fut33v'
 
 TOKEN_FILENAME = "data/token"
 BOTAN_TOKEN_FILENAME = "data/botan_token"
+WEATHER_COM_TOKEN_FILENAME = "data/weather_com_token"
 
 class NovgorodWeatherBot(TelegramBot):
     COMMAND_GET_WEATHER = "/getweather"
@@ -19,8 +22,9 @@ class NovgorodWeatherBot(TelegramBot):
     COMMAND_ADD_TIMER = "/addtimer"
 
     COMMAND_GET_FORECAST = "/getforecast"
+    COMMAND_GET_FORECAST_WEATHER_COM = "/getforecastW"
 
-    def __init__(self, token, name, botan_token=None):
+    def __init__(self, token, name, weather_com_token=None, botan_token=None):
         TelegramBot.__init__(self, token, name, botan_token)
 
         self.add_command_with_parameter(self.COMMAND_ADD_TIMER)
@@ -32,6 +36,12 @@ class NovgorodWeatherBot(TelegramBot):
         self.add_command_no_parameter(self.COMMAND_GET_WIND)
 
         self.add_command_no_parameter(self.COMMAND_GET_FORECAST)
+        self.add_command_no_parameter(self.COMMAND_GET_FORECAST_WEATHER_COM)
+
+        if weather_com_token is not None:
+            self._weather_com_forecaster = WeatherComForecaster(weather_com_token)
+        else:
+            self._weather_com_forecaster = None
 
     def _process_message(self, chat_id, text):
         if text in self._commands_no_parameter:
@@ -68,33 +78,39 @@ class NovgorodWeatherBot(TelegramBot):
             if w:
                 return w
         if self._check_message_for_command(text, self.COMMAND_GET_FORECAST):
-            f = novgorod_forecast.get_forecast()
+            f = YandexForecaster.get_forecast()
             if f:
                 return f
             else:
                 return u"Проблемы при получении прогноза погоды"
+        if self._check_message_for_command(text, self.COMMAND_GET_FORECAST_WEATHER_COM):
+            if self._weather_com_forecaster is not None:
+                f = self._weather_com_forecaster.get_forecast()
+                if f:
+                    return f
+                else:
+                    return u"Проблемы при получении прогноза погоды"
         return False
 
     def _get_start_message(self):
         return """
         Погода в великом Новгороде
-        Погода — Новгород.ру
-        http://novgorod.ru/weather
-        Прогноз — Яндекс.Погода
-        Логотип бота:
-        vk.com/mzzaxixart
-        Автор бота:
-        ilya.fut33v.ru/contacts
-
         Команды:
         /start, /help
         /getweather — Погода
         /getforecast — Прогноз
+
+        Погода — [Новгород.ру](http://novgorod.ru/weather)
+        Прогноз — Яндекс.Погода, Weather.com
+        [Логотип бота](vk.com/mzzaxixart)
+        [Автор бота](ilya.fut33v.ru/contacts), Telegram: @fut33v
+        [Github](https://github.com/fut33v/NovgorodWeatherBot)
         """
 
 
 if __name__ == "__main__":
     t = bot_util.read_one_string_file(TOKEN_FILENAME)
     botan_t = bot_util.read_one_string_file(BOTAN_TOKEN_FILENAME)
-    bot = NovgorodWeatherBot(t, name="NovgorodWeatherBot", botan_token=botan_t)
+    weather_com_t = bot_util.read_one_string_file(WEATHER_COM_TOKEN_FILENAME)
+    bot = NovgorodWeatherBot(t, name="NovgorodWeatherBot", botan_token=botan_t, weather_com_token=weather_com_t)
     bot.start_poll()
