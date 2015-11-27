3d3d7e562589966c23c12596aa1b4aace3a2ca6b
fut33v <fat32nov@gmail.com>
2015-11-27 22:37:11 +0300

    kek

--- novgorod_weather_bot.py
+++ novgorod_weather_bot.py
@@ -24,6 +24,8 @@ class NovgorodWeatherBot(TelegramBot):
     COMMAND_GET_FORECAST = "/getforecast"
     COMMAND_GET_FORECAST_WEATHER_COM = "/getforecastweathercom"
 
+    COMMAND_GET_BUTTHUR = "/getbutthurt"
+
     def __init__(self, token, name, weather_com_token=None, botan_token=None):
         TelegramBot.__init__(self, token, name, botan_token)
 
@@ -37,6 +39,8 @@ class NovgorodWeatherBot(TelegramBot):
 
         self.add_command_no_parameter(self.COMMAND_GET_FORECAST)
         self.add_command_no_parameter(self.COMMAND_GET_FORECAST_WEATHER_COM)
+
+        self.add_command_no_parameter(self.COMMAND_GET_BUTTHUR)
 
         if weather_com_token is not None:
             self._weather_com_forecaster = WeatherComForecaster(weather_com_token)
@@ -90,6 +94,8 @@ class NovgorodWeatherBot(TelegramBot):
                     return f
                 else:
                     return u"Проблемы при получении прогноза погоды"
+        if self._check_message_for_command(text, self.COMMAND_GET_BUTTHUR):
+            return "https://pp.vk.me/c629309/v629309903/209b8/a22Q1yCTn4s.jpg"
         return False
 
     def _get_start_message(self):
