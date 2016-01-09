2d2c324879c53964786b4258b801a2d228d1745f
Ilya Fateev <fat32nov@gmail.com>
2016-01-10 02:00:52 +0300

    Fixed script checking and running bot: using right directory

--- scripts/check_bot.sh
+++ scripts/check_bot.sh
@@ -1,10 +1,10 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 regexp_for_check="[n]ovgorod_weather_bot.py"
-check_bot=`ps aux | grep "[n]ovgorod_weather_bot.py" | wc -l`
-# echo kek: $check_bot
+check_bot=`ps aux | grep $regexp_for_check | wc -l`
 if [ $check_bot -gt 1 ] 
     then
         echo Found $check_bot process\[es\] with regexp: \"$regexp_for_check\"
     exit 0
 fi
-
-screen -d -m python novgorod_weather_bot.py
+echo Not found bot process with regexp $regexp_for_check
+screen -d -m python $DIR/../novgorod_weather_bot.py
