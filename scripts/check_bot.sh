2967216a3967b98e17138cdd95210ff44d56aacf
Ilya Fateev <fat32nov@gmail.com>
2016-01-15 22:47:43 +0300

    check_bot.sh little improved, crontab example added, not needed scripts removed

--- scripts/check_bot.sh
+++ scripts/check_bot.sh
@@ -1,10 +1,23 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-regexp_for_check="[n]ovgorod_weather_bot.py"
-check_bot=`ps aux | grep $regexp_for_check | wc -l`
-if [ $check_bot -gt 1 ] 
+#!/bin/bash
+
+# That script checks is script for NovgorodWeatherBot running, if not, it runs it
+# $1 - is directory with NovgorodWeatherBot script
+
+SCRIPT_DIR=$1
+echo $1 and $SCRIPT_DIR
+SCRIPT_NAME="novgorod_weather_bot.py"
+REGEXP_FOR_CHECK="[n]ovgorod_weather_bot.py"
+
+check_bot=`ps aux | grep $REGEXP_FOR_CHECK | wc -l`
+
+if [ $check_bot -gt 0 ] 
     then
-        echo Found $check_bot process\[es\] with regexp: \"$regexp_for_check\"
+        echo Found $check_bot process\[es\] with regexp: \"$REGEXP_FOR_CHECK\"
     exit 0
 fi
-echo Not found bot process with regexp $regexp_for_check
-screen -d -m python $DIR/../novgorod_weather_bot.py
+
+echo Not found bot process with regexp $REGEXP_FOR_CHECK
+
+echo Now cd to $SCRIPT_DIR and run $SCRIPT_NAME
+cd $SCRIPT_DIR
+python $SCRIPT_NAME
--- scripts/start_bot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-screen -d -m python novgorod_weather_bot.py
-
